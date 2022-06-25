de09b282bcbd3d1ff6eca0c070075fb6590063fa
koritacrah <juanmcoro2003@gmail.com>
2022-06-25 15:30:22 -0300

    Arreglos generales

--- ProyBackendGrupo07/models/anuncio.js
+++ ProyBackendGrupo07/models/anuncio.js
@@ -4,11 +4,11 @@ const {Schema} = mongoose;
 const AnuncioSchema = new Schema({
    texto: {type: String, required: true},
    tipo: {type: String, required: true},
-   medio: {type: String, required: true},
+   medio: {type: [String], required: true},
    fechaEntrada: {type: String, required: false},
    fechaSalida: {type: String, required: false},
    estado: {type: String, required: false},
-   destinatarios: {type: Object, required: 'debe asignar a quien esta dirigido el anuncio'},
+   destinatarios: {type: [Schema.Types.ObjectId], required: 'debe asignar a quien esta dirigido el anuncio'},
    recurso: {type:String, required: true},
    tiempoLectura: {type:String, required:false},
    redactor: {type: Schema.Types.ObjectId, ref: "Empleado", required: true},
--- ProyBackendGrupo07/models/area.js
+++ ProyBackendGrupo07/models/area.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const AreaSchema = new Schema({
     nombreArea: { type: String, required: true },
     encargado: { type: Schema.Types.ObjectId, ref: "Empleado"}
+    
 });
 
 module.exports = mongoose.models.Area || mongoose.model('Area', AreaSchema);
