c0fd081e17344ab128b71d048b15a59d193ec333
koritacrah <juanmcoro2003@gmail.com>
2022-06-30 23:57:02 -0300

    Filtros de anuncio para busquedas avanzadas

--- ProyBackendGrupo07/models/anuncio.js
+++ ProyBackendGrupo07/models/anuncio.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const AnuncioSchema = new Schema({
-   texto: {type: String, required: true},
+   texto: {type: String, required: false},
    tipo: {type: String, required: true},
    medio: {type: [String], required: true},
-   fechaEntrada: {type: String, required: false},
-   fechaSalida: {type: String, required: false},
+   fechaEntrada: {type: Date, required: false},
+   fechaSalida: {type: Date, required: false},
    estado: {type: String, required: false},
-   destinatarios: {type: [String], required: 'debe asignar a quien esta dirigido el anuncio'},//destinatarios: {type: [Schema.Types.ObjectId], required: 'debe asignar a quien esta dirigido el anuncio'},
+   destinatarios: {type: [Schema.Types.ObjectId], required: 'debe asignar a quien esta dirigido el anuncio'},
    recursos: {type: [String], required: true},
    tiempoLectura: {type:String, required:false},
-   redactor: {type: String, required: true}, //{type: Schema.Types.ObjectId, ref: "Empleado", required: true},
+   redactor: {type: Schema.Types.ObjectId, ref: "Empleado", required: false},
+   fechaCreacion: {type: Date, required: true},
 });
-
-
 
 module.exports = mongoose.models.Anuncio || mongoose.model('Anuncio', AnuncioSchema);
