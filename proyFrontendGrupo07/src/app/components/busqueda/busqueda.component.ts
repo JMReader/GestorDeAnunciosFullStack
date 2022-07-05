96d667ca5a2de54861569aca6f3a0d3093fc4018
Marcos Rosales <marcostadeorosales@gmail.com>
2022-07-05 05:51:56 -0300

    Update busqueda.component.ts

--- proyFrontendGrupo07/src/app/components/busqueda/busqueda.component.ts
+++ proyFrontendGrupo07/src/app/components/busqueda/busqueda.component.ts
@@ -14,6 +14,14 @@ export class BusquedaComponent implements OnInit {
 
   filtrosDisponibles: Array<string> = [];
   anuncios = new Array<Anuncio>();
+  destinatarioSelected: boolean = false;
+  fechasSelected: boolean = false;
+  medioSelected: boolean = false;
+  tituloSelected: boolean = false;
+  tipoSelected: boolean = false;
+  estadoSelected: boolean = false;
+  redactorSelected: boolean = false;
+
   //DROPDOWN FILTRO
   dataFiltros: Array<ElementForList> = new Array<ElementForList>();//{ item_id: number, item_text: string }
   settingsFiltros: IDropdownSettings = {
@@ -28,7 +36,7 @@ export class BusquedaComponent implements OnInit {
     destinatarios: new FormControl([]),//[], Validators.required
     fechaStart: new FormControl(),
     fechaEnd: new FormControl(),
-    medios: new FormControl([]),//[], Validators.required
+    medio: new FormControl([]),//[], Validators.required
     titulo: new FormControl(),
     tipo: new FormControl(),
     estado: new FormControl(),
@@ -36,15 +44,15 @@ export class BusquedaComponent implements OnInit {
   })
 
 
-  constructor(private as: AnunciosService) { 
+  constructor(private as: AnunciosService) {
 
   }
 
-  obtenerAnuncios(){
+  obtenerAnuncios() {
     this.anuncios = new Array<Anuncio>();
     this.as.getAnuncios().subscribe(result => {
       console.log(result);
-      result.forEach((element : any) => {
+      result.forEach((element: any) => {
         var unAnuncio = new Anuncio();
         Object.assign(unAnuncio, element);
         this.anuncios.push(unAnuncio);
@@ -52,12 +60,12 @@ export class BusquedaComponent implements OnInit {
     });
   }
 
-  async cargarFiltros(){
+  async cargarFiltros() {
     //destinatario, fechas, medio de publicación, texto, tipo de contenido,estado, redactor
-    this.filtrosDisponibles= new Array<string>();
+    this.filtrosDisponibles = new Array<string>();
     this.filtrosDisponibles.push("Destinatario");
     this.filtrosDisponibles.push("Fechas");
-    this.filtrosDisponibles.push("Medios");
+    this.filtrosDisponibles.push("Medio");
     this.filtrosDisponibles.push("Titulo");
     this.filtrosDisponibles.push("Tipo");
     this.filtrosDisponibles.push("Estado");
@@ -78,113 +86,143 @@ export class BusquedaComponent implements OnInit {
     });
     await new Promise(f => setTimeout(f, 90));
     console.log(this.dataFiltros);
-    
+
     //this.displayMedios=true;
   }
 
-  cambioFiltro(){
+  cambioFiltro() {
     var filtrosElegidos = this.filtroForm.get('filtros')?.value;
     filtrosElegidos.forEach(async (element: any) => {
 
-    switch (element.item_id){
-      case "Fechas":{
-        this.filtroForm.get('fechaStart')?.setValidators(Validators.required);
-        this.filtroForm.get('fechaStart')?.setValue(null);
+      switch (element.item_id) {
+        case "Fechas": {
+          this.filtroForm.get('fechaStart')?.setValidators(Validators.required);
+          this.filtroForm.get('fechaStart')?.setValue(null);
 
-        this.filtroForm.get('fechaEnd')?.setValidators(Validators.required);
-        this.filtroForm.get('fechaEnd')?.setValue(null);
-        break;
+          this.filtroForm.get('fechaEnd')?.setValidators(Validators.required);
+          this.filtroForm.get('fechaEnd')?.setValue(null);
+          this.fechasSelected=true;
+          break;
+        }
+        case "Medios": {
+          this.filtroForm.get('medio')?.setValidators(Validators.required);
+          this.filtroForm.get('medio')?.setValue(null);
+          this.medioSelected = true;
+          break;
+        }
+        case "Titulo": {
+          this.filtroForm.get('titulo')?.setValidators(Validators.required);
+          this.filtroForm.get('titulo')?.setValue(null);
+          this.tituloSelected = true;
+          break;
+        }
+        case "Tipo": {
+          this.filtroForm.get('tipo')?.setValidators(Validators.required);
+          this.filtroForm.get('tipo')?.setValue(null);
+          this.tipoSelected = true;
+          break;
+        }
+        case "Estado": {
+          this.filtroForm.get('estado')?.setValidators(Validators.required);
+          this.filtroForm.get('estado')?.setValue(null);
+          this.estadoSelected = true;
+          break;
+        }
+        case "Redactor": {
+          this.filtroForm.get('redactor')?.setValidators(Validators.required);
+          this.filtroForm.get('redactor')?.setValue(null);
+          this.redactorSelected = true;
+          break;
+        }
+        case "Destinatario": {
+          this.filtroForm.get('destinatario')?.setValidators(Validators.required);
+          this.filtroForm.get('destinatario')?.setValue(null);
+          this.destinatarioSelected = true;
+          break;
+        }
       }
-      case "Medios":{
-        this.filtroForm.get('medios')?.setValidators(Validators.required);
-        this.filtroForm.get('medios')?.setValue(null);
-        break;
-      }
-      case "Titulo":{
-        this.filtroForm.get('titulo')?.setValidators(Validators.required);
-        this.filtroForm.get('titulo')?.setValue(null);
-        break;
-      }
-      case "Tipo":{
-        this.filtroForm.get('tipo')?.setValidators(Validators.required);
-        this.filtroForm.get('tipo')?.setValue(null);
-        break;
-      }
-      case "Estado":{
-        this.filtroForm.get('estado')?.setValidators(Validators.required);
-        this.filtroForm.get('estado')?.setValue(null);
-        break;
-      }
-      case "Redactor":{
-        this.filtroForm.get('redactor')?.setValidators(Validators.required);
-        this.filtroForm.get('redactor')?.setValue(null);
-        break;
-      }
-    } 
-    await new Promise(f => setTimeout(f, 60));
+      await new Promise(f => setTimeout(f, 60));
     });
   }
 
-  filtrar(){
+  filtrar() {
     var filtrosElegidos = this.filtroForm.get('filtros')?.value;
     var anunciosAFiltrar = this.anuncios;
     var anunciosEncontrados = true;
     filtrosElegidos.forEach(async (element: any) => {
 
-      switch (element.item_id){
-        case "Fechas":{
+      switch (element.item_id) {
+        case "Fechas": {
           var fechaInicio = this.filtroForm.get('fechaStart')?.value;
           var fechaFin = this.filtroForm.get('fechaEnd')?.value;
           await new Promise(f => setTimeout(f, 30));
           // this.destinatarios = this.destinatarios.filter(o => { return o.areaAsignada._id === area._id }).slice();
-          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.fechaCreacion > fechaInicio && o.fechaCreacion < fechaFin});
+          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.fechaCreacion > fechaInicio && o.fechaCreacion < fechaFin });
           break;
         }
-        case "Medios":{ //El unico que dudo si funcara o no :c
-          var mediosElegidos = this.filtroForm.get('medios')?.value;
+        case "Medios": { //El unico que dudo si funcara o no :c
+          var medioElegido = this.filtroForm.get('medio')?.value;
+          var anuncios = new Array<Anuncio>();
 
-          mediosElegidos.forEach((medio : any) => {
-            if (anunciosAFiltrar.find(medio) == undefined){
-                anunciosEncontrados = false;
-            }
+          anunciosAFiltrar.forEach((anuncio: Anuncio) => {
+            anuncio.medios.forEach(element => {
+              if (element == medioElegido) {
+                anuncios.push(anuncio);
+              }
+            });
           });
           await new Promise(f => setTimeout(f, 90));
-          if (anunciosEncontrados == true){
-            anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.medios == mediosElegidos});
-          }
-          //anunciosAFiltrar.find(mediosElegidos);
+          anunciosAFiltrar = anuncios;
+          await new Promise(f => setTimeout(f, 90));
           break;
         }
-        case "Titulo":{
+        case "Titulo": {
           var titulo = this.filtroForm.get('titulo')?.value;
           await new Promise(f => setTimeout(f, 30));
-          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.titulo == titulo});
+          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.titulo == titulo });
           break;
         }
-        case "Tipo":{
+        case "Tipo": {
           var tipo = this.filtroForm.get('tipo')?.value;
           await new Promise(f => setTimeout(f, 30));
-          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.tipo == tipo});
+          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.tipo == tipo });
           break;
         }
-        case "Estado":{
-          var estado =this.filtroForm.get('estado')?.value;
+        case "Estado": {
+          var estado = this.filtroForm.get('estado')?.value;
           await new Promise(f => setTimeout(f, 30));
-          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.estado == estado});
+          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.estado == estado });
           break;
         }
-        case "Redactor":{
+        case "Redactor": {
           var redactor = this.filtroForm.get('redactor')?.value;//valor ID
           await new Promise(f => setTimeout(f, 30));
-          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.redactor._id == redactor});
+          anunciosAFiltrar = anunciosAFiltrar.filter(o => { return o.redactor._id == redactor });
           break;
         }
-      } 
+        case "Destinatario": {
+          var destinatario = this.filtroForm.get('destinatario')?.value;//valor ID
+          await new Promise(f => setTimeout(f, 30));
+          var anuncios = new Array<Anuncio>();
+
+          anunciosAFiltrar.forEach((anuncio: Anuncio) => {
+            anuncio.destinatarios.forEach(element => {
+              if ( element == destinatario) {
+                anuncios.push(anuncio);
+              }
+            });
+          });
+          await new Promise(f => setTimeout(f, 90));
+          anunciosAFiltrar = anuncios;
+          await new Promise(f => setTimeout(f, 90));
+          break;
+        }
+      }
       await new Promise(f => setTimeout(f, 80));
-      });
+    });
 
   }
-  
+
 
   ngOnInit(): void {
   }
