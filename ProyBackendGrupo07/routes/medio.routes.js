e315e77b2e6bbddbe9f7efd9afa15fbedc0a495c
Lucas Mesconi <mesconilucas2877@gmail.com>
2022-07-05 13:11:44 -0300

    a
    a

--- ProyBackendGrupo07/routes/medio.routes.js
+++ ProyBackendGrupo07/routes/medio.routes.js
@@ -1,9 +1,10 @@
 const medioCtrl = require('./../controllers/medio.controller');
+const autCtrl = require('./../controllers/auth.controller');
 
 const express = require('express');
 const router = express.Router();
 
 router.get('/obtener', medioCtrl.getMedios);
-router.post('/crear', medioCtrl.createMedio);
+router.post('/crear', autCtrl.verifyToken, medioCtrl.createMedio);
 
 module.exports = router;
